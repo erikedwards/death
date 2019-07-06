dbdb3b3c79333b3eb694f40e00761e0a94e6ee26
erikedwards <erik.p.edwards@gmail.com>
2019-07-06 13:23:02 -0700

    first working

--- cell.py
+++ cell.py
@@ -1,5 +1,16 @@
+import random
+import pygame
 
 class Cell:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+    def __init__(self, col, row, size):
+        self.col = col
+        self.row = row
+        self.x = col * size
+        self.y = row * size
+        self.rect = pygame.Rect(self.x, self.y, size, size)
+        doa = random.randint(0, 1)
+        self.alive = False
+        if doa == 0:
+            self.alive = True
+        self.age = 0
+        self.umph = 0
--- death.py
+++ death.py
@@ -1,6 +1,7 @@
 import pygame
 import math
-from Cell import cell
+import numpy
+from cell import Cell
 
 # CONSTANTS
 SCREEN_WIDTH = 1600
@@ -22,21 +23,69 @@ FONT = pygame.font.SysFont('Courier New', 30)
 FONT_RGB = RED
 
 # init game values
-size = 20
-cols = int(SCREEN_WIDTH / size)
-rows = int(SCREEN_HEIGHT / size)
-grid = [[]]
-
-
+cellSize = 20
+cols = int(SCREEN_WIDTH / cellSize)
+rows = int(SCREEN_HEIGHT / cellSize)
+grid = numpy.empty([cols, rows], Cell)
 
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
-# Main Loop
-run = True
-
 # set frame rate
 FR = 60
+
+# fill grid with initial values.
+def init_grid():
+    for i in range(cols):
+        for j in range(rows):
+            grid[i,j] = Cell(i, j, cellSize)
+
+
+def draw_grid():
+    for i in range(cols):
+        for j in range(rows):
+            c = grid[i, j]
+            if c.alive:
+                cell_color = WHITE
+            else:
+                cell_color = BLACK
+            pygame.draw.rect(screen, cell_color, c.rect)
+
+def update_grid():
+    new_grid = grid
+    for i in range(cols):
+        for j in range(rows):
+            n = count_neighbors(i, j)
+            # if DEAD and exactly 3 neighbors --> ALIVE
+            if not grid[i,j].alive and n == 3:
+                new_grid[i,j].alive = True
+            # if ALIVE and 1 or less neighbors --> DEAD
+            if grid[i,j].alive and n < 2:
+                new_grid[i,j].alive = False
+            # if ALIVE and 4 or more neighbors --> DEAD
+            if grid[i,j].alive and n > 3:
+                new_grid[i,j].alive = False
+            # otherwise, no change from last generation
+            new_grid[i,j].age += 1
+
+
+def count_neighbors(x, y):
+    count = 0
+    # check all eight neighbors
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if grid[(x + i + cols) % cols, (y + j + rows) % rows].alive:
+                count += 1
+    # but you also counted yourself so remove one if you're alive
+    if grid[x, y].alive:
+        count -= 1
+    return count
+
+
+
+# Main Loop
+run = True
+init_grid()
 
 while run:
     for event in pygame.event.get():
@@ -59,10 +108,16 @@ while run:
             #         FR -= 1
             continue
 
-    # Game logic
-
     # Clear Screen
     screen.fill(WHITE)
+
+    # Game logic
+    update_grid()
+
+    # draw stuff
+    draw_grid()
+
+
 
 
     # --- Update the screen
@@ -71,5 +126,5 @@ while run:
     # --- Limit to 60 frames per second
     clock.tick(FR)
 
-    # Close the window and quit.
+# Close the window and quit.
 pygame.quit()
